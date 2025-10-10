a4d0695bf5ab1e7117140d0959a3bfff08968751
nyaivu <marvtfs@gmail.com>
2025-10-10 18:26:39 +0800

    Removed Supabase/ssr Dependencies

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,8 +1,5 @@
 import AuthGuard from "@/components/AuthGuard";
 import DashboardNavBar from "@/components/DashboardNavBar";
-import { useSessionStore } from "@/stores/sessionStore";
-import { createClient } from "@/utils/supabase/server";
-import { redirect } from "next/navigation";
 
 const DashboardLayout = async ({
   children,
