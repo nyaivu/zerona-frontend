f6d59185408dd71d1dba9647d4b659fd7bc738ad
nyaivu <marvtfs@gmail.com>
2025-10-11 11:11:05 +0800

    Removed Kursus & Murid from Navbar

--- src/components/DashboardNavBar.tsx
+++ src/components/DashboardNavBar.tsx
@@ -29,22 +29,6 @@ const DashboardNavBar = () => {
           </Link>
         </li>
         <li>
-          <Link
-            className="flex flex-row items-center gap-2"
-            href="/dashboard/courses"
-          >
-            <p>Kursus</p>
-          </Link>
-        </li>
-        <li>
-          <Link
-            className="flex flex-row items-center gap-2"
-            href="/dashboard/students"
-          >
-            <p>Murid</p>
-          </Link>
-        </li>
-        <li>
           <button onClick={() => clearSession()}>Logout</button>
         </li>
       </ul>
@@ -86,52 +70,6 @@ const DashboardNavBar = () => {
               />
             </svg>
             <p>Dashboard</p>
-          </Link>
-        </li>
-        <li className="w-full">
-          <Link
-            className="flex flex-row items-center gap-2"
-            href="/dashboard/courses"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25"
-              />
-            </svg>
-
-            <p>Kursus</p>
-          </Link>
-        </li>
-        <li className="w-full">
-          <Link
-            className="flex flex-row items-center gap-2"
-            href="/dashboard/students"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5"
-              />
-            </svg>
-
-            <p>Murid</p>
           </Link>
         </li>
         <li className="w-full">
